41f90fd0cbb1934c58201f82b92433d8ad4f56d6
mbm1337 <mikkelsen96@live.dk>
2024-04-02 11:42:15 +0200

    add endpoints

--- src/main/java/org/main/Main.java
+++ src/main/java/org/main/Main.java
@@ -1,7 +1,71 @@
 package org.main;
 
+import io.javalin.apibuilder.EndpointGroup;
+
+import static io.javalin.apibuilder.ApiBuilder.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+
+    public static EndpointGroup getEventRoutes() {
+        return () -> {
+            path("events", () -> {
+                get("/", ctx ->{} );
+                get("/:id", ctx ->{});
+                post("/", ctx ->{});
+                put("/:id", ctx ->{});
+                delete("/:id", ctx ->{});
+
+            });
+        };
+    }
+
+    public static EndpointGroup getUserRoutes() {
+        return () -> {
+            path("users", () -> {
+                get("/", ctx ->{});
+                get("/:id", ctx ->{});
+                post("/", ctx ->{});
+                put("/:id", ctx ->{});
+                delete("/:id", ctx ->{});
+
+            });
+        };
+    }
+
+    public static EndpointGroup getRegistrationRoutes() {
+        return () -> {
+            path("registrations", () -> {
+                get("/:id", ctx ->{});
+                post("/:id", ctx ->{});
+                delete("/:id", ctx ->{});
+
+            });
+            path("registration", () -> {
+                get("/:id", ctx ->{});
+            });
+        };
+    }
+
+    public static EndpointGroup getAuthenticationRoutes() {
+        return () -> {
+            path("login", () -> {
+                post("/", ctx ->{});
+            });
+            path("logout", () -> {
+                post("/", ctx ->{});
+            });
+            path("register", () -> {
+                post("/", ctx ->{});
+            });
+            path("reset-password", () -> {
+                post("/", ctx ->{});
+            });
+
+        };
+    }
+
 }
